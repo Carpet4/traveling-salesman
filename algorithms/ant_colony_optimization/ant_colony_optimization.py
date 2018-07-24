f4e857a98369e95eca20e5c4487fa4d6ecc45a17
Maayan Blum <maayanblm@gmail.com>
2018-07-24 13:50:54 +0300

    more progress

--- algorithms/ant_colony_optimization/ant_colony_optimization.py
+++ algorithms/ant_colony_optimization/ant_colony_optimization.py
@@ -2,24 +2,45 @@
 import numpy as np
 from utils import get_distance_matrix
 
-def ant_colony_optimization(scenario):
-    pheromones = get_initial_pheromones()
+def ant_colony_optimization(scenario, evaporation_speed = 1/10):
+    #
 
-    while(True):  # TODO: replace true with some condition
+    # evaporation_speed: the ratio in which new pheromones replace
+    # old ones in each time step
+
+    # a matrix holding the distance between each two nodes
+    edge_lengths = get_distance_matrix(scenario)
+
+    # pheromone amount in each edge
+    pheromones = get_initial_pheromones(edge_lengths)
+
+    while not did_converge(pheromones):  # TODO: replace true with some condition
+        # let the ants go wild and release pheromones
         ant_routes = release_ants(pheromones)
-        pheromone_update_matrix = calculate_produced_pheromones(ant_routes)
+        pheromone_update_matrix = calculate_produced_pheromones(ant_routes, edge_lengths)
+
+        # normalize the new pheromones and add them to the total pheromones
+        pheromone_update_matrix /= pheromone_update_matrix.sum() * scenario.shape[0] * evaporation_speed
         pheromones += pheromone_update_matrix
+
+        # normalize the pheromones to avoid overflow
         pheromones /= pheromones.sum() * scenario.shape[0]
 
+    return pheromones_to_route(pheromones)
 
-def get_initial_pheromones(scenario):
+
+def get_initial_pheromones(edge_lengths):
     # using the distances between nodes as initial pheromone values
-    pheromones = get_distance_matrix(scenario)
+    pheromones = edge_lengths.copy()
     # emptying the lower left triangle of the array
-    pheromones[np.tril_indices(scenario.shape[0], -1)] = 0
+    pheromones[np.tril_indices(edge_lengths.shape[0], -1)] = 0
 
     return pheromones
 
+
+def release_ants(pheromones, num_ants):
+    # let n ants walk and produce routes
+    return [produce_ant_route(pheromones) for _ in range(num_ants)]
 
 
 def produce_ant_route(pheromones):
@@ -30,6 +51,25 @@ def produce_ant_route(pheromones):
         route.append(next_node)
 
     return route
+
+
+def calculate_produced_pheromones(ant_routes, edge_lengths):
+    # array to hold the new pheromones
+    produced_pheromones = np.zeros(edge_lengths.shape)
+    # the length of each route taken by an ant
+    route_lengths = edge_lengths[ant_routes, np.roll(ant_routes, 1, axis=1)].sum(axis=1)
+    # for each route, add pheromones to the its edges.
+    # the longer the route, the less pheromones each edge gets
+    for ant_route, route_length in zip(ant_routes, route_lengths):
+        produced_pheromones[ant_route, np.roll(ant_route, 1)] += 1 / route_length
+
+    # since the array is a mirror of it self on the diagonal, add the lower left pheromones
+    # to the upper right, and then zero-out the lower left
+    right_triangle_indices = np.triu_indices(edge_lengths.shape[0], 1)
+    produced_pheromones[right_triangle_indices] += produced_pheromones.T[right_triangle_indices]
+    produced_pheromones.T[right_triangle_indices] = 0
+
+    return produced_pheromones
 
 
 def get_next_node(pheromones, route):
